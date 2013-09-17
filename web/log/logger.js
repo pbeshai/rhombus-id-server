0a723a0ff584b57cec6bd5aadb458717e0e46b8b
Peter Beshai <peter.beshai@gmail.com>
2013-09-16 22:18:54 -0700

    logging updates

--- web/log/logger.js
+++ web/log/logger.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({ json: false, timestamp: true, level: "debug" }),
+    new (winston.transports.Console)({ json: false, timestamp: true, level: "debug", colorize: true }),
     new winston.transports.File({ filename: __dirname + '/server.log.json', json: true, level: "debug", name: "file#json" }),
     new winston.transports.File({ filename: __dirname + '/server.log', json: false, level: "debug" })
   ],
