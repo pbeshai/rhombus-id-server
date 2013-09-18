207f08dfbc11494506fd3287ee40ba646f98fbee
Peter Beshai <peter.beshai@gmail.com>
2013-09-18 16:43:43 -0700

    Better timestamps in console logs

--- web/log/logger.js
+++ web/log/logger.js
@@ -1,13 +1,18 @@
-var winston = require('winston');
+var winston = require('winston'),
+  moment = require("moment");
+
+function timestamp() {
+  return moment().format("HH:mm:ss");
+}
 
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({ json: false, timestamp: true, level: "debug", colorize: true }),
+    new (winston.transports.Console)({ json: false, timestamp: timestamp, level: "debug", colorize: true }),
     new winston.transports.File({ filename: __dirname + '/server.log.json', json: true, level: "debug", name: "file#json" }),
     new winston.transports.File({ filename: __dirname + '/server.log', json: false, level: "debug" })
   ],
   exceptionHandlers: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
+    new (winston.transports.Console)({ json: false, timestamp: timestamp }),
     new winston.transports.File({ filename: __dirname + '/exceptions.log', json: true })
   ],
   exitOnError: false
